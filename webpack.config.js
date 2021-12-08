0b1a1a9e15617f1189f1a715738359211b58b5a5
Nowadays <herespieceofmine@gmail.com>
2021-12-08 14:26:00 +0100

    feat: add formatter + basic webpack conf

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Design guideline',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+};
