cbe8cb1e3375bcc2128d400c000862af6e75efd0
Nowadays <herespieceofmine@gmail.com>
2021-12-27 18:32:22 +0100

    feat: add rules for assets

--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,14 @@ module.exports = {
         test: /\.(sa|sc|c)ss$/i,
         use: [devMode ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
