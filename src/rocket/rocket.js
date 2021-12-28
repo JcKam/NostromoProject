5f35663e5cd12b81b72c32bc19cadb42779bb48a
Nowadays <herespieceofmine@gmail.com>
2021-12-28 12:49:17 +0100

    fix: automatic html handling + add dummy example files

--- src/rocket/rocket.js
+++ src/rocket/rocket.js
@@ -1,1 +1,1 @@
-import './rocket.scss'
+import './rocket.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-const path = require('path');
+const glob = require('glob');
+const path = require('path/posix');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const devMode = process.env.NODE_ENV !== 'production';
@@ -7,38 +8,53 @@ module.exports = {
   entry: {
     rocket: './src/rocket/rocket.js',
     aviato: './src/aviato/aviato.js',
-    nostromo: './src/nostromo/nostromo.js'
+    nostromo: './src/nostromo/nostromo.js',
   },
   output: {
-    filename: 'js/[name].bundle.js',
+    filename: '[name]/[name].[hash].js',
     path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'static/[hash][ext][query]',
     clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: 'kit-ui-rocket.html',
+      filename: 'rocket/index.html',
       template: path.resolve(__dirname, 'src', 'rocket', 'index.html'),
-      chunks: ['rocket']
+      chunks: ['rocket'],
     }),
     new HtmlWebpackPlugin({
-      filename: 'kit-ui-aviato.html',
+      filename: 'aviato/index.html',
       template: path.resolve(__dirname, 'src', 'aviato', 'index.html'),
-      chunks: ['aviato']
+      chunks: ['aviato'],
     }),
     new HtmlWebpackPlugin({
-      filename: 'index.html',
+      filename: 'nostromo/index.html',
       template: path.resolve(__dirname, 'src', 'nostromo', 'index.html'),
-      chunks: ['nostromo']
-    })
-  ].concat(
-    devMode
-      ? []
-      : [
-          new MiniCssExtractPlugin({
-            filename: 'css/[name].css',
-          }),
-        ]
-  ),
+      chunks: ['nostromo'],
+    }),
+  ]
+    .concat(
+      glob.sync('src/**/!(index).html').map((file) => {
+        const pathSplit = file.split(path.sep);
+        pathSplit.shift(); // remove src/
+        const chunkName = pathSplit[0];
+        const filename = pathSplit.join(path.sep);
+        return new HtmlWebpackPlugin({
+          filename,
+          template: path.join(__dirname, 'src', filename),
+          chunks: [chunkName],
+        });
+      })
+    )
+    .concat(
+      devMode
+        ? []
+        : [
+            new MiniCssExtractPlugin({
+              filename: '[name]/[name].css',
+            }),
+          ]
+    ),
   module: {
     rules: [
       {
@@ -47,17 +63,26 @@ module.exports = {
       },
       {
         test: /\.(sa|sc|c)ss$/i,
-        use: [devMode ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader','sass-loader'],
+        use: [devMode ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'static/images/[name].[hash][ext][query]',
+        },
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'static/fonts/[name].[hash][ext][query]',
+        },
       },
     ],
   },
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  devServer: {
+    open: ['/nostromo/index.html'],
+  },
 };
