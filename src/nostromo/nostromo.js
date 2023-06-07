f157f314d4ef04d39c7a93a6a52b624361f7fe7b
JcKam <artwhenjc@gmail.com>
2023-06-07 17:49:37 +0200

    feat: Evol version beat lengow

--- src/nostromo/nostromo.js
+++ src/nostromo/nostromo.js
@@ -1,3 +1,9 @@
 import './nostromo.scss'
+import './button.scss'
+import './matching-categories.html'
+import './index.html'
+import './matching-attributs.html'
+import './marketplace-multi_pays.html'
+import './upsell.html'
 
 console.log("test")
